ad1922587b763e2daa298c4134478a29fd5e7096
sunhaoyang <sunchen2138@gmail.com>
2019-07-14 21:10:27 +0200

    made changes

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'blog/homepage.html', {})
